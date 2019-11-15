6afe52a3d449e07d9fa2b40f7524076ece5d98f7
Thiroux-Collard Jonathan <thirouxjonathan@yahoo.fr>
2019-11-15 11:36:07 +0100

    simplification of insertion method

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -2,6 +2,10 @@ public class LinkedList<T extends Comparable<T>>
 {
     private ListItem<T> head;
 
+    /*
+     * This method add a new element on the linkedlist.
+     * If head point is null, the new element is located on the head else we insert from the head.
+     */
     public void add(T data) {
         if (head == null) {
             head = new Item(data);
@@ -11,54 +15,40 @@ public class LinkedList<T extends Comparable<T>>
         }
     }
 
-    private void insert(ListItem item, T data){
-        if(item.getNext() != null){
-            int compare = item.compareTo(data);
-            if(compare < 0){
+    /*
+     * This methode compare the current item value and value to add.
+     * If the item value is lower than parameter value we add the new data to next case else we rotate elements.
+     */
+    private void insert(ListItem<T> item, T data){
+        int compare = item.compareTo(data);
+        if(compare < 0){
+            if(item.getNext() != null) {
                 insert(item.getNext(), data);
             }
-            if(compare > 0){
-                // make a new Item
+            else{
                 Item newItem = new Item(data);
-                newItem.setNext(item);
-
-                if(item.getPrevious() != null){
-                    newItem.setPrevious(item.getPrevious());
-                    // configure old item
-                    item.getPrevious().setNext(newItem);
-                }
-                else{
-                    head = newItem;
-                    item.setPrevious(newItem);
-                }
-            }
-        }
-        else {
-            int compare = item.compareTo(data);
-            if (compare < 0){
-                // add the new item at the end of list
-                Item newItem = new Item(data);
-                //configure link between new Item and currentItem
                 newItem.setPrevious(item);
                 item.setNext(newItem);
             }
-            if(compare > 0){
-                Item newItem = new Item(data);
-                newItem.setNext(item);
+        }
+        if(compare > 0){
+            Item newItem = new Item(data);
+            newItem.setNext(item);
 
-                if(item.getPrevious() != null) {
-                    newItem.setPrevious(item.getPrevious());
-                    // configure old item
-                    item.getPrevious().setNext(newItem);
-                }
-                else {
-                    head = newItem;
-                    item.setPrevious(newItem);
-                }
+            if(item.getPrevious() != null){
+                newItem.setPrevious(item.getPrevious());
+                item.getPrevious().setNext(newItem);
+            }
+            else{
+                head = newItem;
+                item.setPrevious(newItem);
             }
         }
     }
 
+    /*
+     * This methode print the linkedlist on the screen.
+     */
     public void printList() {
         ListItem item = head;
         while (item != null) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<>();
-        list.add("Zidane");
-        list.add("Henri");
-        list.add("Caroline");
-        list.add("Elodie");
-        list.printList();
+        LinkedList<String> list1 = new LinkedList<>();
+        list1.add("Zidane");
+        list1.add("Henri");
+        list1.add("Caroline");
+        list1.add("Elodie");
+        list1.printList();
 
-        /*LinkedList<Integer> list = new LinkedList<>();
-        list.add(10);
-        list.add(100);
-        list.add(5);
-        list.add(45);
-        list.printList();*/
+        LinkedList<Integer> list2 = new LinkedList<>();
+        list2.add(10);
+        list2.add(100);
+        list2.add(5);
+        list2.add(45);
+        list2.printList();
     }
 }
